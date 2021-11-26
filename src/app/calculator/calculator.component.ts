a65225ac180c55510c396fd940fc0dcb132806bb
SaMajeste237 <83540031+SaMajeste237@users.noreply.github.com>
2021-11-26 17:38:09 +0100

    firt commit

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   currentNumber = '0';
-  firstOperand :any;
-  operator:any;
+  firstOperand :any= null ;
+  operator:any= null;
   waitForSecondNumber = false;
 
   constructor() { }
